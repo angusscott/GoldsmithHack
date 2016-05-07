1cfb8ecd793ee0d92b7d5937c9d5d65bae2600b8
Selby <david@selbys.org.uk>
2016-05-07 21:41:03 +0100

    Implement frequency modulation

--- js/audio.js
+++ js/audio.js
@@ -7,35 +7,35 @@ var data = [5, 7, 1, 10, 11, 20, 15, 1, 20],
     masterVolume = context.createGain(),
     startTime = context.currentTime,
     frequency = 493.883, // 'B' note
-    duration = 1;
-
-// for(var i=0; i<10; i++) data.push(i);
-// for(var i=10; i>0; i--) data.push(i);
+    duration = 10;
 
 var maxvol = 0.3,
-    minvol = 0.01;
+    minvol = 0.01,
+    minfreq = frequency-5,
+    maxfreq = frequency+5;
 
 // Scale data (domain) to volume interval (range)
-var linearScale = function(x) { // What about logarithmic?
+var linearScale = function(x, range0, range1) { // What about logarithmic?
     var a = Math.min.apply(null, data);
     var b = Math.max.apply(null, data);
-    var c = minvol;
-    var d = maxvol;
+    var c = range0;
+    var d = range1;
     return(c*(x-a)/(b-a) + d*(x-a)/(b-a));
 };
 
-var lengthOfClip = 10;
+var interval = duration/data.length,
+    volumes = [],
+    frequencies = [];
 
-var interval = lengthOfClip/data.length;
-volumes = [];
 for(var i=0; i<data.length; i++) {
-    volumes.push(linearScale(data[i]));
+    volumes.push(linearScale(data[i], minvol, maxvol));
+    frequencies.push(linearScale(data[i], minfreq, maxfreq));
 };
 
 // Set the volume not too loud
 masterVolume.gain.value = 0.1;
 
-// Create wave oscillators
+// Create wave oscillator
 var osc = context.createOscillator();
 
 // Set oscillator wave type
@@ -45,24 +45,19 @@ osc.type = 'sawtooth';
 osc.connect(masterVolume);
 masterVolume.connect(context.destination);
 
-// Detune oscillators for chorus effect
-// osc1.frequency.value = frequency + data['counts'][0]/2;
-// osc2.frequency.value = frequency + data['counts'][1]/2;
-
-// Change volume according to data
+// Change volume and frequency according to data
 var timepoint = startTime;
 for(var i = 0; i<data.length; i++) {
-    console.log(volumes[i]);
-    // Discrete jumps
-    //masterVolume.gain.setValueAtTime(volumes[i], timepoint);
     // Linear interpolation (line graph)
     masterVolume.gain.linearRampToValueAtTime(volumes[i], timepoint);
+    // Frequency adjustment
+    osc.frequency.linearRampToValueAtTime(frequencies[i], timepoint);
     timepoint = timepoint + interval;
 };
 
 // Fade out
-masterVolume.gain.setValueAtTime(0.1, startTime + lengthOfClip - 0.1);
-masterVolume.gain.linearRampToValueAtTime(0, startTime + lengthOfClip);
+masterVolume.gain.setValueAtTime(0.1, startTime + duration - 0.1);
+masterVolume.gain.linearRampToValueAtTime(0, startTime + duration);
 
 // Start oscillator
 osc.start(startTime);
