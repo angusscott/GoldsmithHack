b7fde0355908612a59e5969469ba9a6439ac29ad
angusscott <angusscott@me.com>
2016-05-07 22:08:08 +0100

    Separated Javascript from index.html

---
+++ js/getAudio.js
@@ -0,0 +1,35 @@
+(function () {
+    var audio = new Audio();
+
+    function searchTracks(query) {
+        $.ajax({
+            url: 'https://api.spotify.com/v1/search',
+            data: {
+                q: query,
+                type: 'track'
+            },
+            success: function (response) {
+                if (response.tracks.items.length) {
+                    var track = response.tracks.items[0];
+                    audio.src = track.preview_url;
+                    audio.play();
+                    communicateAction('<div>Playing ' + track.name + ' by ' + track.artists[0].name + '</div><img width="150" src="' + track.album.images[1].url + '">');
+                }
+            }
+        });
+    }
+
+    function playSong(songName, artistName) {
+        var query = songName;
+        if (artistName) {
+            query += ' artist:' + artistName;
+        }
+
+        searchTracks(query);
+    }
+
+    $( "#playmusic" ).click(function() {
+        playSong("Sandstorm", "Darude");
+    });
+
+})();
