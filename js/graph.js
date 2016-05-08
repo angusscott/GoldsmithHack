530c87515c1359941cc84ad30c754e583a7c683b
angusscott <angusscott@me.com>
2016-05-08 10:30:05 +0100

    Have getAudio now returning correctly

--- js/graph.js
+++ js/graph.js
@@ -20,34 +20,43 @@ var line = d3.svg.line()
     .x(function(d, i) {return x(i);})
     .y(function(d) { return y(d); });
 
-var svg = d3.select("#graphContainer").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var createGraph = function(dataCollection){
+    var svg = d3.select("#graphContainer").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
-    x.domain([0,dataset.length]); //Get index in array
-    y.domain(d3.extent(dataset, function(d) { return d }));
+        x.domain([0,dataset.length]); //Get index in array
+        y.domain(d3.extent(dataset, function(d) { return d }));
 
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis)
-        .attr("y", 6)
-        .attr("dy", ".71em");
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis)
+            .attr("y", 6)
+            .attr("dy", ".71em");
 
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em");
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em");
 
-    svg.append("path")
-        .datum(dataset)
-        .attr("class", "line")
-        .attr("d", line);
+        svg.append("path")
+            .datum(dataset)
+            .attr("class", "line")
+            .attr("d", line);
+}
 
+createGraph()
 
+$( "#dataInputBox" ).focusout(function() {
+    d3.select("#graphContainer").html("");
+    var entryList = $( "#dataInputBox" ).val()
+    dataset = JSON.parse("[".concat(entryList).concat(']'))
+    createGraph()
+});
